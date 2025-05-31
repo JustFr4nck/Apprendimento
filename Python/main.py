4437b744dceb6555156bf048309a3e6f3b4d4cf3
JustFr4nck <francyperry07@gmail.com>
2025-05-31 23:17:30 +0200

    modifica 3

--- Python/main.py
+++ Python/main.py
@@ -5,3 +5,17 @@ print("Benvenuto nel mio programma!!!")
 nome = input("Inserisci il tuo nome: ")
 print(f"Benvenuto {nome}!!")
 
+cognome = input("Inserire cognome: ")
+print("cognome inserito")
+
+num = input("Inserire numero di telefono: ")
+if num.isdigit() == True: 
+    print("Numero di telefono registrato")
+else:
+    print()
+
+mail = input("Inserire mail: ")
+print("mail registrata")
+
+print("___I tuoi dati___")
+print(f"Nome: {nome}\nCognome: {cognome}\nNumero di telefono: {num}\nEmail: {mail}")
