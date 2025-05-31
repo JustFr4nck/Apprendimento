7ca91b4c4963f3b3bf8edbd7a42ddc3e7639aa4e
JustFr4nck <francyperry07@gmail.com>
2025-05-31 20:39:10 +0200

    Tutorial py 1

---
+++ main.py
@@ -0,0 +1,37 @@
+
+#___BASI___
+# [print] per stampare
+# [print(f"hello {x}")] la f permette di mettre fra 2 {} una variabile da stampare
+# per dichiarare una variabile non serve mettere il tipo di variabile che vai a realizzare basta fare variabile = contenuto(String, Float, Integer, Boolean)
+# per le condizioni basta mettere i [:] per iniziare ad inserire le condizioni
+# con [print(type(variabile))] si stampa il tipo della variabile
+# (TypeCasting) per convertire una variabile ad un altro tipo si usano le funzioni [str(), int(), float(), bool()]
+# con [input(string)] puoi richieder l'imput dall'utente su terminale e tra le parentesi puoi mettere una stringa che verrà stampata a schermo
+#[pass] place holder
+
+#___Funzioni Matematiche___
+# [round(variabile)] si usa per arrotondare un valore
+# [abs(variabile)] si usa per modificare una variabile al suo valore assoluto
+# [pow(variabile, valore elevato)] si usa per calcolare la potenza di una variabile
+# [max(variabile1, variabile2, variabile3)] ricerca della variabile più grande (Se ne possono mettere all'infinito)
+# [min(variabile1, variabile2, variabile3)] ricerca della variabile più piccola (Se ne possono mettere all'infinito)
+
+#___ class math___
+# [import math] si importa la classe math per delle funzioni specifiche
+# [math.pi] pi greco
+# [math.e] esponenziale
+# [math.sqrt(valori sotto radice)] radice quadrata
+# [math.ceil(variabile)] approssima per eccesso
+# [math.floor(variabile)] approssima per difetto
+
+#___ Operatori Logici___
+# [or] [and] [not]
+
+#___String functions___
+# [len(variabile)] si usa per calcolare la lunghezza di una stringa (compresi gli spazi)(restituisce integer)
+# [variabile.find(carattere)] si usa per cercare un carattere in una stringa (Restituisce la posizione del primo carattere corrispondente) (puoi cercare anche gli spazi), in caso non trova il carattere restituisce -1 (restituisce integer)
+# [variabile.rfind(carattere)] si usa per cercare un carattere in una stringa (Restituisce la posizione dell'ultimo carattere corrispondente) (puoi cercare anche gli spazi), in caso non trova il carattere restituisce -1 (restituisce integer)
+# [variabile.capitalize()] rende la prima lettera di una stringa maiuscola
+# [variabile.upper()] rende una stringa tutta maiuscola
+# [variabile.lower()] rende una stringa tutta minuscola
+# [variabile.isdigit()] restituisce [True] se la stringa è composta SOLO da numeri, altrimenti restituisce [False]
