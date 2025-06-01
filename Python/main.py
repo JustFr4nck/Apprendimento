8876bc430b3bad5862bc37a12729dd9e62f0b37d
JustFr4nck <francyperry07@gmail.com>
2025-06-01 09:33:41 +0200

    Aggiunta while

--- Python/main.py
+++ Python/main.py
@@ -8,11 +8,13 @@ print(f"Benvenuto {nome}!!")
 cognome = input("Inserire cognome: ")
 print("cognome inserito")
 
-num = input("Inserire numero di telefono: ")
-if num.isdigit() == True: 
-    print("Numero di telefono registrato")
-else:
-    print()
+num = "w"
+while num.isdigit() == False or len(num) != 10:
+    num = input("Inserire numero di telefono: ")
+    if num.isdigit() == True and len(num) == 10: 
+        print("Numero di telefono registrato")
+    else:
+        print("ERRORE: digitare solo numeri nel limite della lunghezza di 10")
 
 mail = input("Inserire mail: ")
 print("mail registrata")
